c0b52c7a6601dacabaa58d1775317a7cdeb50dde
natsu727 <himajin0717@gmail.com>
2024-10-10 19:15:12 +0900

    first commit

---
+++ app/workspace/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from '@mui/material'
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button variant='contained'><AttachMoneyIcon/>Button<AttachMoneyIcon/></Button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
